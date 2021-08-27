36a6db08ceb0d16b2f1e2a1935b781dbe7d2ace2
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-27 15:01:06 -0500

    create a function that appends our data to the html table

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ website that allows users to input weather data for a city
 - [x] html table with placeholder data
 - [x] two input fields to collect some data
 - [x] add a button that calls a function
-- [] create a function that appends our data to the html table
+- [x] create a function that appends our data to the html table
 - [] logic to highlight specific fields on that table
 - [] style the able
 - [] ???
--- client.js
+++ client.js
@@ -1,15 +1,22 @@
-console.log('JS ready');
+// console.log('JS ready');
 
 $(document).ready(ReadyNow);
 
 function ReadyNow() {
-    console.log('JQ ready');
+    // console.log('JQ ready');
     // click handler
-    $('.submit-button').on('click', appendCityToTable);
+    $('#submit-button').on('click', appendCityToTable);
 }
 
 function appendCityToTable() {
     let cityName = $('#city-name').val(); // .val() returns a string
-    let temperature = $('city-temperature').val();
+    let temperature = $('#city-temperature').val();
     console.log(cityName, temperature);  
+
+    $('#city-table-body').append(`
+        <tr>
+            <td>${cityName}</td>
+            <td>${temperature}</td>
+        </tr>
+    `)
 }
